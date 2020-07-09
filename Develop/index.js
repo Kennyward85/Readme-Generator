5ba6b4796ed641dc978b8891bbb7436fb4258953
Osyrus85 <osyrus85@gmail.com>
2020-07-09 17:08:40 -0500

    Read me Generator functioning need to update personal readme and add clip

--- Develop/index.js
+++ Develop/index.js
@@ -1,15 +1,57 @@
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+const fs = require("fs");
+const path = require("path");
+
+
 // array of questions for user
 const questions = [
-
+    {
+        message: "What is your Project Title.",
+        name: "Title"
+    },
+    {
+        message: "What is Project Description.",
+        name: "Description"
+    }, 
+    {
+        message: "What are the neccessary steps for installation.",
+        name: "Install"
+    }, 
+    {
+        message: "How will this app be used",
+        name: "Use"
+    }, 
+    {
+        message: "What type of license will be used",
+        name: "License"
+    },
+    {
+        message: "What types of Contributions were made",
+        name: "Contributions"
+    }, 
+    {
+        message: "What is required to make it run Tests",
+        name: "Tests"
+    }, 
+    {
+        message: "For questions contact me here",
+        name: "Questions"
+    },
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    console.log(data);
+    return fs.writeFileSync(path.join(process.cwd(),fileName),data);
 }
 
 // function to initialize program
 function init() {
-
+inquirer.prompt(questions)
+.then(function(response) {
+    writeToFile("Readme.md", generateMarkdown(response));
+});
 }
 
 // function call to initialize program
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,33 @@
+
+
+
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.Title}
 
+  # Description
+  ${data.Description}
+
+  # Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  # Installation
+  ${data.Install}
+  # Usage
+  ${data.Use}
+  # License
+  ${data.License}
+  # Contributing
+  ${data.Contributions}
+  # Tests
+  ${data.Tests}
+  # Questions
+  ${data.Questions}
 `;
 }
 
